f7e349de9706f44befe8ee5ee265d7ce14ababa6
Mohammad Atif <mhdatif0511@gmail.com>
2026-02-09 13:45:55 +0530

    frontend setup complete

---
+++ frontend/src/context/cartContext.jsx
@@ -0,0 +1,60 @@
+import { useState, createContext, useContext } from "react";
+import { api } from "../lib/axios.js";
+
+const CartContext = createContext();
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState({});
+
+    //frontend only
+    const addToCart = (productId, qty = 1) => {
+        setCart((prev) => ({
+            ...prev,
+            [productId]: (prev[productId] || 0) + qty
+        }));
+    };
+
+    const updateQuantity = (productId, qty) => {
+        if (qty <= 0) {
+            removeItem(productId);
+            return;
+        }
+        setCart((prev) => ({ ...prev, [productId]: qty }));
+    };
+
+    const removeItem = (productId) => {
+        setCart((prev) => {
+            const next = { ...prev };
+            delete next[productId];
+            return next;
+        });
+    };
+
+    const submitCart = async () => {
+        const items = Object.entries(cart).map(
+            ([product, quantity]) => ({
+                product,
+                quantity
+            })
+        );
+        await api.post("/cart", { items });
+
+        // Frontend erases cart
+        setCart({});
+    };
+
+    return (
+        <CartContext.Provider
+            value={{
+                cart,
+                addToCart,
+                updateQuantity,
+                removeItem,
+                submitCart
+            }}
+        >
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+export const useCart = () => useContext(CartContext);
